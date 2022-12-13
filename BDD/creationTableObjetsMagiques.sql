0410d9fe8bda5d8cce0e8897a6a49ef15b4ca74e
Natounet <naytan@protonmail.com>
2022-12-13 01:20:28 +0100

    Modification des objets magiques

--- BDD/creationTableObjetsMagiques.sql
+++ BDD/creationTableObjetsMagiques.sql
@@ -7,9 +7,9 @@ CREATE TABLE IF NOT EXISTS objetsMagiques(
     description varchar(1000), -- description de l'objet magique ( peut être nul )
     prix int, -- prix de l'objet magique, peut être null si non achetable
     histoire varchar(1000), -- Histoire de l'objet, peut être nul
-    localisation varchar(30) not null, -- 
+    localisation varchar(300) not null, -- lore
     utlisation varchar(10) not null, -- jour/semaine/mois/année/heure
-    spam boolean not null, -- vrai / faux
+    spam boolean not null, -- vrai / faux ( peut être utilisable à l'infini)
     coutPsyche int not null, -- coût en psyché
     coutVie int not null, -- coût en vie
     caracteristique varchar(20) NOT NULL, -- une seule caractéristique ( ex puissance )

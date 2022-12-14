6e462d9a5c68d7bbc99d2ef69fcc97970a7c52ee
ptirem <ptirem72@gmail.com>
2022-12-14 21:39:08 +0100

    continuation of store.js

--- web-server/src/store.js
+++ web-server/src/store.js
@@ -4,8 +4,8 @@ var denomination
 var niveau
 var age 
 var peuple =""
-var peuple_caracteristique=[[0,0,0,0,0,0],[0,0,0,0,0,0]]
-var peuple_caracteristique_bonus=[0,0,0,0,0,0]
+var peuple_caracteristique=[[false,false,false,false,false,false,false],[false,false,false,false,false,false,false]]
+var caracteristique=[0,0,0,0,0,0,0]
 var langues
 var heritage
 var taille
@@ -14,8 +14,15 @@ var pv  //7 pts a repartir entre pv et psy
 var psy
 var magie
 var arme
-var caracteristique=[0,0,0,0,0,0]
 var position
+var a_repartir=[]
+
+const nom_caracterisique=["Puissance","Finesse","Vigueur","Savoir","L'instinct","Social","Stress"]
+const indice_cara={"puissance":0,"Finesse":1,"Vigueur":2,"Savoir":3,"L'instinct":4,"Social":5,"Stress":6}
+const bonus=0
+const malus=1
+
+
 
 
 
@@ -23,9 +30,17 @@ var position
 function peuple_malus_bonus(){
     switch(peuple){
         case "nain":
-            []
-        
-        
+            pv=21
+            psy=8
+            a_repartir[bonus]=[{"nb_point":7,}]
+
+
+            a_repartir[bonus].forEach(element => {
+                peuple_caracteristique[bonus][indice_cara[element]]=true
+            });
+            a_repartir[malus].forEach(element => {
+                peuple_caracteristique[malus][indice_cara[element]]=true
+            });
         break;
 
 

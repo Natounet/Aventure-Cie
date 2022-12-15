71354ab13e29379b0df9ed64dbf337a0e5ffe951
Natounet <naytan@protonmail.com>
2022-12-16 00:38:50 +0100

    Rectification d'une erreur dans la table ObjetsMagiques

--- BDD/creationTableObjetsMagiques.sql
+++ BDD/creationTableObjetsMagiques.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS objetsMagiques(
     prix float, -- prix de l'objet magique, peut être null si non achetable
     histoire text, -- Histoire de l'objet, peut être nul
     localisation text, -- lore
-    utlisation varchar(100) not null, -- jour/semaine/mois/année/heure
+    utilisation varchar(100) not null, -- jour/semaine/mois/année/heure
     spam boolean not null, -- vrai / faux ( peut être utilisable à l'infini)
     coutPsyche int not null, -- coût en psyché, peut être nul
     coutVie int not null, -- coût en vie, peut être nul

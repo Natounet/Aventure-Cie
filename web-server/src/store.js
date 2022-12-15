f5a8f91434418e7c5aa0480112412f784fe79d3b
ptirem <ptirem72@gmail.com>
2022-12-15 01:03:24 +0100

    add a modification_peopl class

--- web-server/src/store.js
+++ web-server/src/store.js
@@ -1,10 +1,11 @@
+import { init } from 'svelte/internal';
 import { writable } from 'svelte/store';
 
 var denomination
 var niveau
 var age 
 var peuple =""
-var peuple_caracteristique=[[false,false,false,false,false,false,false],[false,false,false,false,false,false,false]]
+var peuple_caracteristique=[]
 var caracteristique=[0,0,0,0,0,0,0]
 var langues
 var heritage
@@ -17,12 +18,29 @@ var arme
 var position
 var a_repartir=[]
 
-const nom_caracterisique=["Puissance","Finesse","Vigueur","Savoir","L'instinct","Social","Stress"]
-const indice_cara={"puissance":0,"Finesse":1,"Vigueur":2,"Savoir":3,"L'instinct":4,"Social":5,"Stress":6}
+const nom_caracterisique=["Puissance","Finesse","Vigueur","Savoir","instinct","Social","Stress"]
+const indice_cara={"Puissance":0,"Finesse":1,"Vigueur":2,"Savoir":3,"instinct":4,"Social":5,"Stress":6}
 const bonus=0
 const malus=1
-
-
+class modification_people{
+    constructor(nb_point, characteristics){
+        this.nb_point =nb_point;
+        this.characteristics =characteristics;
+        this.index_char={"Puissance":0,"Finesse":1,"Vigueur":2,"Savoir":3,"instinct":4,"Social":5,"Stress":6}
+        this.tab_possibilitys=creation_tab_possibilitys(characteristics,this.index_char)
+        this.isbonus=isbonus_method(nb_point)
+    }
+    static creation_tab_possibilitys(characteristics_args,index_char_args) {
+        var tab=new Array(7).fill(false)
+        characteristics_args.forEach(element => {
+            tab[index_char_args[element]]=true
+        });
+        return tab
+    }
+    static isbonus_method(nb_point_args){
+        return nb_point_args>=0;
+}
+}
 
 
 
@@ -32,15 +50,11 @@ function peuple_malus_bonus(){
         case "nain":
             pv=21
             psy=8
-            a_repartir[bonus]=[{"nb_point":7,}]
+            a_repartir=[new modification_people(-10,["Puissance","Finesse","Savoir","Social"]),new modification_people(20,["Savoir","Social"])]
 
 
-            a_repartir[bonus].forEach(element => {
-                peuple_caracteristique[bonus][indice_cara[element]]=true
-            });
-            a_repartir[malus].forEach(element => {
-                peuple_caracteristique[malus][indice_cara[element]]=true
-            });
+            
+                    
         break;
 
 

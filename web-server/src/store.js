9712bb8027d2526f9c217376df18c4f4c7a68c46
ptirem <ptirem72@gmail.com>
2022-12-15 18:48:29 +0100

    ajout d'une structure personnage

--- web-server/src/store.js
+++ web-server/src/store.js
@@ -1,21 +1,7 @@
 import { init } from 'svelte/internal';
 import { writable } from 'svelte/store';
 
-var denomination
-var niveau
-var age 
-var peuple =""
-var peuple_caracteristique=[]
-var caracteristique=[0,0,0,0,0,0,0]
-var langues
-var heritage
-var taille
-var poid
-var pv  //7 pts a repartir entre pv et psy  
-var psy
-var magie
-var arme
-var position
+var psy,pv 
 var a_repartir=[]
 
 const nom_caracterisique=["Puissance","Finesse","Vigueur","Savoir","instinct","Social","Stress"]
@@ -41,28 +27,50 @@ class modification_people{
         return nb_point_args>=0;
 }
 }
-
-
-
-
-function peuple_malus_bonus(){
-    switch(peuple){
-        case "nain":
-            pv=21
-            psy=8
-            a_repartir=[new modification_people(-10,["Puissance","Finesse","Savoir","Social"]),new modification_people(20,["Savoir","Social"])]
-
-
-            
-                    
-        break;
-
-
+class attribut{
+    constructor(name,description,bonus,characteristics_affecte) {
+        this.name_atribut=name;
+        this.name_atribut=description;
+        this.bonus_points=bonus;
+        this.isbonus=isbonus_method(bonus);
+        this.characteristics_affecte=characteristics_affecte
     }
-
-    
-    
+    static isbonus_method(bonus_args){
+        return i>=0;
+    }
 }
+class personnage{
+    constructor(peuple){
+        this.peuple=peuple;
+        this.pv;
+        this.ps;
+        this.a_repartir;
+        this.attribut=[];
+        this.characteristics=["Puissance","Finesse","Vigueur","Savoir","instinct","Social","Stress"]
+    }
+    static configuration_pour_peuple(peuple){
+        switch(peuple){
+            case "nain":
+                this.pv=21
+                this.psy=8
+                this.a_repartir=[new modification_people(-10,["Puissance","Finesse","Savoir","Social"]),new modification_people(20,["Savoir","Social"])]
+                this.ajout_attribut("vision dans le noir","Un savoir-faire dans une compétence  d’Instinc",0,[])
+                this.ajout_attribut(new attribut("Stabilit"," Grâce à sa corpulence, un nain possède  +5% au test de Puissance et de Vigueur pour un jet d’oppositio",5,["Puissance","Vigueur"]))
+                this.ajout_attribut(new attribut("Nemesis","Le nain choisit un peuple qui sera considéré  comme son ennemi juré. Il gagne un bonus de +5% à ses tests contre les protagonistes de cette nation.",5,this.characteristics))
+                this.ajout_attribut(new attribut("Robustess"," Encaisse, sans prendre de dégâts, la  première attaque physique reçue par comba",0,[]))
+                break;
+            
+        }
+    }
+    static ajout_attribut(name,description,point,characteristics_affectes){
+        this.attribut.push(new attribut(name,description,point,characteristics_affectes))
+    }
+}
+
+
+
+
+
 
 
 

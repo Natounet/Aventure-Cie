2cc113908b04a3a332c545c65385f79f1c898970
Natounet <naytan@protonmail.com>
2022-12-16 00:00:54 +0100

    Modification de la structure de la BDD, ObjetMagique ( CoutPsyche et CoutVie deviennent requis )

--- BDD/creationBDD.sql
+++ BDD/creationBDD.sql
@@ -73,11 +73,11 @@ CREATE TABLE IF NOT EXISTS objetsMagiques(
     description text, -- description de l'objet magique ( peut être nul )
     prix float, -- prix de l'objet magique, peut être null si non achetable
     histoire text, -- Histoire de l'objet, peut être nul
-    localisation text not null, -- lore
+    localisation text, -- lore
     utlisation varchar(100) not null, -- jour/semaine/mois/année/heure
     spam boolean not null, -- vrai / faux ( peut être utilisable à l'infini)
-    coutPsyche int, -- coût en psyché, peut être nul
-    coutVie int, -- coût en vie, peut être nul
+    coutPsyche int not null, -- coût en psyché, peut être nul
+    coutVie int not null, -- coût en vie, peut être nul
     caracteristique varchar(100) NOT NULL, -- une seule caractéristique ( ex puissance )
     typeObjet varchar(100) NOT NULL , -- exemple : lunette, bottes, ...
     nomBalise varchar(100), -- Id du don si existant

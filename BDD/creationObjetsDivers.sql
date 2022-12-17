5b7e093dc11a63fc90afeb9dd1b16d8de240bb6b
Natounet <naytan@protonmail.com>
2022-12-17 13:13:43 +0100

    Création de la table objetsDivers

---
+++ BDD/creationObjetsDivers.sql
@@ -0,0 +1,11 @@
+use compagnieCie;
+
+CREATE TABLE IF NOT EXISTS objetsDivers(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY , -- ID de l'objet divers
+    nom varchar(30) NOT NULL, -- Dénomination de 'l'objet
+    description text, -- Description de l'objet
+    prix int, -- Prix de l'objet
+    taille float not null, -- Taille de l'objet
+    poids float not null, -- Poids de l'objet
+    armure int, -- Points d'armure dans des cas précis
+)
